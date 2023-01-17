acbd1567280e6a15a8b2b3a4075cb3389e7b5699
shenhua <695675124@qq.com>
2023-01-17 02:01:35 -0800

    http.c;

---
+++ http.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <ctype.h>
+#include <arpa/inet.h>
+
+#define SERVER_PORT 80
+
+int main(void){
+  return 0;
+}
